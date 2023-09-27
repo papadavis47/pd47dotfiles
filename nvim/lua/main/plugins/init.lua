82bb1296ba62d0ead986d3a4c883298dadc59841
John William Davis <jwdavisdeveloper@gmail.com>
2023-09-27 11:03:45 -0700

    add zenburn colorscheme option for occasional use

--- nvim/lua/main/plugins/init.lua
+++ nvim/lua/main/plugins/init.lua
@@ -1,20 +1,21 @@
 return {
--- Colorschemes
-  'rafamadriz/neon',
-  'dracula/vim',
-  'folke/tokyonight.nvim',
-  'fenetikm/falcon',
-  'rebelot/kanagawa.nvim',
+	-- Colorschemes
+	"rafamadriz/neon",
+	"dracula/vim",
+	"folke/tokyonight.nvim",
+	"fenetikm/falcon",
+	"rebelot/kanagawa.nvim",
+	"phha/zenburn.nvim",
 
-  'navarasu/onedark.nvim',
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
+	"navarasu/onedark.nvim",
+	-- Git related plugins
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
 
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
 
-  -- This is for navigating with Tmux
-  "christoomey/vim-tmux-navigator",
-  { "folke/neodev.nvim", opts = {} }
+	-- This is for navigating with Tmux
+	"christoomey/vim-tmux-navigator",
+	{ "folke/neodev.nvim", opts = {} },
 }
